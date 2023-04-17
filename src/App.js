5402d3afe5d72ba2fc18513245b60c8f91a1e824
Hothaifa Jaber <i74ifa@icloud.com>
2023-04-17 14:28:09 +0300

    Support is loading state & Pagination "load more"

--- src/App.js
+++ src/App.js
@@ -6,42 +6,115 @@ import MovieCard from "./MovieCard"
 //  aceb8800  bring the code from your email
 const API_URL = "https://www.omdbapi.com?apikey=aceb8800"
 const App = () => {
-    const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState("Batman");
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
+    const [page, setPage] = useState(2);
+    const [hasMore, setHasMore] = useState(true)
   
     useEffect(() => {
-      searchMovies("Batman");
+      searchMovies();
     }, []);
   
-    const searchMovies = async (title) => {
-      const response = await fetch(`${API_URL}&s=${title}`);
-      const data = await response.json();
-  
-      setMovies(data.Search);
+    const searchMovies = async (query = null) => {
+      console.log(searchTerm)
+      setIsLoading(true);
+      const response = await fetch(`${API_URL}&s=${searchTerm}&page=${page}`).then((res) => {
+        setIsLoading(false)
+        return res.json()
+      });
+      const data = response;
+      if (data.Search) {
+        setMovies(data.Search);
+      }
     };
+
+    const searchMoviesMore = async () => {
+      setIsLoadingMore(true);
+      setPage(page+1)
+      console.log(page)
+      const response = await fetch(`${API_URL}&s=${searchTerm}&page=${page}`).then((res) => {
+        setIsLoading(false)
+        setIsLoadingMore(false)
+        return res.json()
+      });
+
+      const allMovies = movies.concat(response.Search)
+      if (response.Search) {
+        setMovies(allMovies)
+      }else {
+        setHasMore(false)
+      }
+
+
+    }
+
+    const onChangeSearch = (e) => {
+      setSearchTerm(e.target.value);
+      setPage(1);
+    }
   
     return (
       <div className="app">
         <h1>MovieLand</h1>
-  
+
         <div className="search">
           <input
             value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={(e) => onChangeSearch(e)}
             placeholder="Search for movies"
           />
-          <img
-            src={SearchIcon}
-            alt="search"
-            onClick={() => searchMovies(searchTerm)}
-          />
+          {!isLoading ? (
+            <img
+              src={SearchIcon}
+              alt="search"
+              onClick={() => searchMovies(searchTerm) | setHasMore(true)}
+            />
+          ) : (
+            <svg
+              className="animate-spin"
+              aria-hidden="true"
+              width="42"
+              height="42"
+              viewBox="0 0 24 24"
+              fill="#f9d3b4"
+            >
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path d="M18.364 5.636L16.95 7.05A7 7 0 1019 12h2a9 9 0 11-2.636-6.364z"></path>
+            </svg>
+          )}
         </div>
 
         {movies?.length > 0 ? (
-          <div className="container">
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
+          <div>
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+            {hasMore ? (
+              <div className="load-more" onClick={searchMoviesMore}>
+                <button>
+                  {isLoadingMore ? (
+                    <svg
+                      className="animate-spin"
+                      aria-hidden="true"
+                      width="26"
+                      height="26"
+                      viewBox="0 0 24 24"
+                      fill="#f9d3b4"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path d="M18.364 5.636L16.95 7.05A7 7 0 1019 12h2a9 9 0 11-2.636-6.364z"></path>
+                    </svg>
+                  ) : null}
+                  Load More
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         ) : (
           <div className="empty">
